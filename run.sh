1889dcbf7224138908ed0f266149f2c7a85c82c4
rtrimana <rtrimana@uci.edu>
2018-03-19 15:36:55 -0700

    More scripts to plot graphs with labels.

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@
 # Basically, we make one call per one device that we want to analyze.
 ROUTER=Router
 ROUTER_MAC=b0:b9:8a:73:69:8f
-#PHONE=Nexus_5_Black
-#PHONE_MAC=64:89:9a:86:a9:7d
-PHONE=Motorola
-PHONE_MAC=a8:96:75:2f:0c:9c
+PHONE=Nexus_5_Black
+PHONE_MAC=64:89:9a:86:a9:7d
+#PHONE=Motorola
+#PHONE_MAC=a8:96:75:2f:0c:9c
 
 #PREFIX=wemo
 #DEVICE=WeMo_Switch
@@ -40,9 +40,16 @@ PHONE_MAC=a8:96:75:2f:0c:9c
 PREFIX=smartthings-plug
 DEVICE=SmartThings_Plug
 DEVICE_MAC=d0:52:a8:a3:60:0f
-PATH_SETUP=/scratch/traffic_measurements/Switches-Feb2018/smartthings/setup
-PATH_LOCAL=/scratch/traffic_measurements/Switches-Feb2018/smartthings/local
-PATH_REMOTE=/scratch/traffic_measurements/Switches-Feb2018/smartthings/remote
+PATH_SETUP=/scratch/traffic_measurements/Switches-Feb2018/smartthings-plug/setup
+PATH_LOCAL=/scratch/traffic_measurements/Switches-Feb2018/smartthings-plug/local
+PATH_REMOTE=/scratch/traffic_measurements/Switches-Feb2018/smartthings-plug/remote
+
+#PREFIX=smartthings-mp-sensor
+#DEVICE=SmartThings_Multipurpose_Sensor
+#DEVICE_MAC=d0:52:a8:a3:60:0f
+#PATH_SETUP=/scratch/traffic_measurements/Switches-Feb2018/smartthings-mp-sensor/setup
+#PATH_LOCAL=/scratch/traffic_measurements/Switches-Feb2018/smartthings-mp-sensor/local
+#PATH_REMOTE=/scratch/traffic_measurements/Switches-Feb2018/smartthings-mp-sensor/remote
 
 PATH_SETUP_WLAN1_JSON=$PREFIX.wlan1.setup.json
 PATH_SETUP_ETH0_JSON=$PREFIX.eth0.setup.json
@@ -61,6 +68,7 @@ PATH_REMOTE_ETH0_JSON=$PREFIX.eth0.remote.json
 PATH_REMOTE_ETH1_DNS_JSON=$PREFIX.eth1.remote.dns.json
 PATH_REMOTE_ETH1_JSON=$PREFIX.eth1.remote.json
 PATH_GNUPLOT=./plot_scripts/plot_ts_graph_$PREFIX
+PATH_GNUPLOT_LABELED=./plot_scripts/plot_ts_graph_$PREFIX\_labeled
 PATH_GNUPLOT_COMBINED=./plot_scripts/plot_ts_graph_$PREFIX\_combined
 PATH_DIR_RESULT=result
 PATH_RESULT_SETUP_WLAN1=$PREFIX\_switch_wlan1_setup
@@ -84,17 +92,18 @@ PATH_RESULT_PHONE_REMOTE_WLAN1=$PREFIX\_phone_wlan1_remote
 #python ./base_gexf_generator.py $PATH_LOCAL/$PATH_LOCAL_ETH0_DNS_JSON $PATH_DIR_RESULT/$PATH_RESULT_LOCAL_ETH0.gexf
 #python ./parser/parse_packet_frequency.py $PATH_LOCAL/$PATH_LOCAL_ETH0_JSON $PATH_DIR_RESULT/$PATH_RESULT_LOCAL_ETH0 $ROUTER $ROUTER_MAC
 #python ./base_gexf_generator.py $PATH_LOCAL/$PATH_LOCAL_ETH1_DNS_JSON $PATH_DIR_RESULT/$PATH_RESULT_LOCAL_ETH1.gexf
-#python ./parser/parse_packet_frequency.py $PATH_LOCAL/$PATH_LOCAL_ETH1_JSON $PATH_DIR_RESULT/$PATH_RESULT_LOCAL_ETH1 $DEVICE $DEVICE_MAC
+python ./parser/parse_packet_frequency.py $PATH_LOCAL/$PATH_LOCAL_ETH1_JSON $PATH_DIR_RESULT/$PATH_RESULT_LOCAL_ETH1 $DEVICE $DEVICE_MAC
 
 #python ./base_gexf_generator.py $PATH_REMOTE/$PATH_REMOTE_WLAN1_DNS_JSON $PATH_DIR_RESULT/$PATH_RESULT_REMOTE_WLAN1.gexf
 #python ./parser/parse_packet_frequency.py $PATH_REMOTE/$PATH_REMOTE_WLAN1_JSON $PATH_DIR_RESULT/$PATH_RESULT_REMOTE_WLAN1 $DEVICE $DEVICE_MAC
 #python ./base_gexf_generator.py $PATH_REMOTE/$PATH_REMOTE_ETH0_DNS_JSON $PATH_DIR_RESULT/$PATH_RESULT_REMOTE_ETH0.gexf
 #python ./parser/parse_packet_frequency.py $PATH_REMOTE/$PATH_REMOTE_ETH0_JSON $PATH_DIR_RESULT/$PATH_RESULT_REMOTE_ETH0 $ROUTER $ROUTER_MAC
 #python ./base_gexf_generator.py $PATH_REMOTE/$PATH_REMOTE_ETH1_DNS_JSON $PATH_DIR_RESULT/$PATH_RESULT_REMOTE_ETH1.gexf
-#python ./parser/parse_packet_frequency.py $PATH_REMOTE/$PATH_REMOTE_ETH1_JSON $PATH_DIR_RESULT/$PATH_RESULT_REMOTE_ETH1 $DEVICE $DEVICE_MAC
+python ./parser/parse_packet_frequency.py $PATH_REMOTE/$PATH_REMOTE_ETH1_JSON $PATH_DIR_RESULT/$PATH_RESULT_REMOTE_ETH1 $DEVICE $DEVICE_MAC
 
 #python ./parser/parse_packet_frequency.py $PATH_LOCAL/$PATH_LOCAL_WLAN1_JSON $PATH_DIR_RESULT/$PATH_RESULT_PHONE_LOCAL_WLAN1 $PHONE $PHONE_MAC
 #python ./parser/parse_packet_frequency.py $PATH_REMOTE/$PATH_REMOTE_WLAN1_JSON $PATH_DIR_RESULT/$PATH_RESULT_PHONE_REMOTE_WLAN1 $PHONE $PHONE_MAC
 
 #gnuplot $PATH_GNUPLOT
-gnuplot $PATH_GNUPLOT_COMBINED
+gnuplot $PATH_GNUPLOT_LABELED   # only the WLAN1/ETH1 part - basically just the device perspective
+#gnuplot $PATH_GNUPLOT_COMBINED
