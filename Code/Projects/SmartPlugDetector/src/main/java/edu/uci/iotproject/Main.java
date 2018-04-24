c8c59ea37986428b0743ce9ad21c74a748071edc
Janus Varmarken <varmarken@gmail.com>
2018-04-24 11:04:38 -0700

    add notes

--- Code/Projects/SmartPlugDetector/src/main/java/edu/uci/iotproject/Main.java
+++ Code/Projects/SmartPlugDetector/src/main/java/edu/uci/iotproject/Main.java
@@ -1,5 +1,8 @@
 package edu.uci.iotproject;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Entry point of the application.
  *
@@ -9,6 +12,20 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("it works");
+    }
+
+    private String cloudIPAddress(String hostName) {
+        if (hostName.equals("events.tplinkra.com"))
+            return 205.251.203.26;
+    }
+
+    // TODO move to separate class
+    // Add parameter that is the trace to be analyzed (most like the pcap library's representation of a flow)
+    public String findPattern(Map<String, List<Integer>> hostnameToPacketLengths, String smartPlugIp) {
+
+        // No difference, output "Complete match"
+        // If difference, output <Packet no, deviation from expected> for each packet
+        return null;
     }
 
 }
