84446f11cd7c1a809b5d1a9b3d720f465aa344ad
rtrimana <rtrimana@uci.edu>
2018-04-24 17:49:45 -0700

    Adding the Kaitai library to parse PCAP files.

--- Code/Projects/SmartPlugDetector/src/main/java/edu/uci/iotproject/Main.java
+++ Code/Projects/SmartPlugDetector/src/main/java/edu/uci/iotproject/Main.java
@@ -7,8 +7,15 @@ package edu.uci.iotproject;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("it works");
+        String file = "/home/rtrimana/pcap_processing/smart_home_traffic/Code/Projects/SmartPlugDetector/pcap/wlan1.local.dns.pcap";
+        
+        try {
+            Pcap data = Pcap.fromFile(file);
+            //data.hdr();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
