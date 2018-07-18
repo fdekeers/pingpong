b3ccf839aa077935e3ce1f0b171eb5f39e20f786
Janus Varmarken <varmarken@gmail.com>
2018-07-17 18:53:30 -0700

    Move PcapProcessingPipeline to different package.

--- Code/Projects/SmartPlugDetector/src/main/java/edu/uci/iotproject/analysis/PcapProcessingPipeline.java
+++ Code/Projects/SmartPlugDetector/src/main/java/edu/uci/iotproject/analysis/PcapProcessingPipeline.java
@@ -1,5 +1,7 @@
-package edu.uci.iotproject;
+package edu.uci.iotproject.analysis;
 
+import edu.uci.iotproject.PcapPacketConsumer;
+import edu.uci.iotproject.PcapReader;
 import org.pcap4j.core.PcapPacket;
 
 import java.util.ArrayList;
@@ -15,10 +17,12 @@ public class PcapProcessingPipeline {
 
     private final PcapReader mPcapReader;
     private final List<PcapPacketConsumer> mPacketConsumers;
+    private final PcapPacketFilter mPacketFilter;
 
-    public PcapProcessingPipeline(PcapReader pcapReader) {
+    public PcapProcessingPipeline(PcapReader pcapReader, PcapPacketFilter packetFilter) {
         mPcapReader = pcapReader;
         mPacketConsumers = new ArrayList<>();
+        mPacketFilter = packetFilter;
     }
 
     public void addPcapPacketConsumer(PcapPacketConsumer packetConsumer) {
@@ -27,9 +31,17 @@ public class PcapProcessingPipeline {
 
     public void executePipeline() {
         PcapPacket packet;
+        long count = 0;
         while ((packet = mPcapReader.readNextPacket()) != null) {
+            if (mPacketFilter != null && !mPacketFilter.shouldIncludePacket(packet)) {
+                continue;
+            }
             for (PcapPacketConsumer consumer : mPacketConsumers) {
                 consumer.consumePacket(packet);
+            }
+            count++;
+            if (count % 1000 == 0) {
+                System.out.println("Processed " + count + " packets");
             }
         }
     }
