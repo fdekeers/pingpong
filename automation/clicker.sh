f3d73543514759adfe8600697f86218f3623f61f
rtrimana <rtrimana@uci.edu>
2018-07-25 16:10:30 -0700

    Adding coordinates for the SmartThings app button.

--- automation/clicker.sh
+++ automation/clicker.sh
@@ -14,10 +14,13 @@ RAN_END=200
 
 for ((i=$BEGIN; i<=$END; i+=$INC));
 do
+	#./adb shell getevent -l - use this command to get the position
 	# TP-Link switch
 	#./adb shell input tap 1002 913
 	# D-Link switch
-	./adb shell input tap 987 346
+	#./adb shell input tap 987 346
+	# SmartThings
+	./adb shell input tap 921 1188
 	#date +%r
 	date +"%m/%d/%Y %r"
 	#RAND=$[( $RANDOM % $RAN_END ) + $RAN_STA]
