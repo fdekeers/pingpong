58254c3ac3c057593f2ba631d776431ab867a1f4
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2024-05-14 14:06:10 +0200

    Corrected two mistakes in comments

--- automation/browser.sh
+++ automation/browser.sh
@@ -4,10 +4,10 @@
 # We just need to set up the timer duration
 
 # Range of random number (in seconds)
-# Five websites
+# 10 websites
 BRO_RAN_STA=0
 BRO_RAN_END=9
-# Browsing interval (every 100-500 seconds)
+# Browsing interval (every 10-500 seconds)
 SLP_RAN_STA=10
 SLP_RAN_END=500
 
